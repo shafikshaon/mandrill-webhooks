d4493ad80db86737b2a7f6b8e29e346e5e8ba31a
Shafikur Rahman <shafikshaon@gmail.com>
2023-03-21 23:40:05 +0600

    Add Webhook View and Receive Event

--- events/views.py
+++ events/views.py
@@ -1,7 +1,29 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 
 class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, "index.html")
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class Webhook(View):
+    def head(self, request, *args, **kwargs):
+        return HttpResponse(status=200)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if "mandrill_events" in request.POST:
+                events = json.loads(request.POST["mandrill_events"])
+                print(events)
+            else:
+                return HttpResponse("Empty events", status=400)
+            return HttpResponse(status=200)
+        except json.JSONDecodeError:
+            return HttpResponse("Invalid JSON data", status=400)
--- mandrill_webhooks/urls.py
+++ mandrill_webhooks/urls.py
@@ -4,9 +4,10 @@ mandrill_webhooks URL Configuration
 from django.contrib import admin
 from django.urls import path
 
-from events.views import Home
+from events.views import Home, Webhook
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", Home.as_view(), name="home"),
+    path("webhook/", Webhook.as_view(), name="webhook"),
 ]
